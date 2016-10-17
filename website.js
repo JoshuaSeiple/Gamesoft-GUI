d4602e0d06489cf5a6f80e8d0f55cb29196c3d26
JoshuaSeiple <jseiple@bancroftschool.org>
2016-10-17 18:32:43 -0400

    added second page

--- website.js
+++ website.js
@@ -12,7 +12,28 @@ lineX += 68;
 rect(45,95, 130, 130);
 rect(45,263, 130, 130);
 rect(243,95, 130, 130);
-//text
+var scenes = [true, false];
+var currScene = 0;
+draw = function() {
+if(mouseIsPressed && mouseX>30 && mouseX<98 && mouseY>45 && mouseY< 80 && scenes[0] === false){
+    background(255, 0, 0);
+    
+  currScene =   
+}
+else if(mouseIsPressed && mouseX>30 && mouseX<98 && mouseY>45 && mouseY< 80){
+    background(255, 0, 0);
+    
+}
+};
+
+
+
+
+
+
+
+
+
 textSize(23);
 fill(136, 0, 255);
 text("GameSoft", 150, 30);
